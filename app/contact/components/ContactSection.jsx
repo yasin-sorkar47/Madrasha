963709914a8900da95c14a3ae69980f481a7e639
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-02-12 17:16:54 +0600

    contact section updated

--- app/contact/components/ContactSection.jsx
+++ app/contact/components/ContactSection.jsx
@@ -6,46 +6,45 @@ export default function ContactSection() {
     <section className="bg-blue-100 h-full sm:h-screen sm:flex sm:items-center">
       <div className="container mx-auto w-3/4 flex flex-col sm:flex-row sm:text-center">
         <div className="order-2 bg-gray-100 p-8 sm:order-2 sm:w-1/2">
-          <h3 className="font-bold mb-1 text-xl">Welcome Back</h3>
-          <p className="text-gray-500 font-semibold m-1">
-            Please enter your details
-          </p>
+          <h3 class="font-bold mb-1 text-xl">Say Hello</h3>
           <form action="">
-            <label
-              className="font-semibold mb-1 block sm:text-left"
-              for="email"
-            >
+            <label class="font-semibold mb-1 block sm:text-left" for="email">
+              Full Name
+            </label>
+            <input
+              class="p-2 mb-2 w-full focus:outline-gray-400"
+              type="text"
+              name="full_name"
+              placeholder="Enter Full Name"
+            />
+            <label class="font-semibold mb-1 block sm:text-left" for="email">
               Email address
             </label>
             <input
-              className="p-2 mb-2 w-full focus:outline-gray-400"
+              class="p-2 mb-2 w-full focus:outline-gray-400"
               type="email"
               name="email"
               placeholder="Enter your email"
             />
-            <label
-              className="font-semibold mb-1 block sm:text-left"
-              for="password"
-            >
-              Password
+            <label class="font-semibold mb-1 block sm:text-left" for="password">
+              Subject
             </label>
             <input
-              className="p-2 mb-3 w-full focus:outline-gray-400"
-              type="password"
-              name="password"
-              placeholder="Enter your password"
+              class="p-2 mb-3 w-full focus:outline-gray-400"
+              type="text"
+              name="subject"
+              placeholder="Enter your email"
             />
+            <textarea
+              class="p-2 mb-4 w-full focus:outline-gray-400"
+              name="text_area"
+              placeholder="Your text here"
+            ></textarea>
             <input
-              className="py-2 block w-full bg-blue-950 rounded text-white font-semibold"
+              class="py-2 block w-full bg-blue-950 rounded text-white font-semibold"
               type="submit"
               value="Sing In"
             />
-            <p className="text-gray-500 mt-3">
-              Dont have a acount
-              <a className="text-blue-500 underline" href="#">
-                sing up
-              </a>
-            </p>
           </form>
         </div>
         <div className="order-1 py-8 sm:order-1 sm:w-1/2">
